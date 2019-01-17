0224348158205a9d267f9c1ef11d8f4b7d7594e4
Kun Wang <ifreedom.cn@gmail.com>
2019-01-17 14:29:17 +0800

    Add unity 5.6.5p4.

---
+++ Casks/unity@5.6.5p4.rb
@@ -0,0 +1,29 @@
+cask 'unity@5.6.5p4' do
+  version '5.6.5p4,10861494ddb7'
+  sha256 :no_check
+
+  url "https://download.unity3d.com/download_unity/10861494ddb7/MacEditorInstaller/Unity.pkg"
+  name 'Unity 5.6.5p4'
+  homepage 'https://unity3d.com/unity/'
+
+  pkg 'Unity.pkg'
+
+  preflight do
+    if File.exist? "/Applications/Unity"
+        FileUtils.move "/Applications/Unity", "/Applications/Unity.temp"
+    end
+  end
+
+  postflight do
+    if File.exist? "/Applications/Unity"
+        FileUtils.move "/Applications/Unity", "/Applications/Unity-5.6.5p4"
+    end
+
+    if File.exist? "/Applications/Unity.temp"
+        FileUtils.move "/Applications/Unity.temp", "/Applications/Unity"
+    end
+  end
+
+  uninstall quit:    'com.unity3d.UnityEditor5.x',
+            delete:  '/Applications/Unity-5.6.5p4'
+end
